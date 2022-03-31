e5495c00e07ec2b5af55db8b302a59c029eb1944
Divya <divya@pacdevelopers.com>
2022-03-31 23:09:33 +0530

    initial commit

---
+++ Assets/script/trigggerplacecontroller.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trigggerplacecontroller : MonoBehaviour
+{
+    public int position;
+    public bool occupied;
+    public string occupiedtext;
+
+}
